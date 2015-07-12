189f5adcc357a482700ce777b7b628fa57eb92a5
fineiskid <ffineis@uw.edu>
2015-07-11 19:26:27 -0700

    HW2.R almost done, finishing prob 4. Changing various included functions

--- ENERGYRISK/homework_files/HW2.R
+++ ENERGYRISK/homework_files/HW2.R
@@ -16,7 +16,7 @@ j.index = seq(from=0, to=6, by=1) #month number, 0 to 6 by 1's
 nbNodes = seq(from=1,to=length(level_x),by=2) #number of nodes at each time point
 
 
-##### Value EUROPEAN PUT Option ####
+##### Problem 1: Value EUROPEAN PUT Option ####
 mult = -1 #put option...
 # i) Fitted forward prices, spot price data fitted correctly
 Forw = exp(as.numeric(a_is[length(j.index)]) + level_x)
@@ -50,7 +50,11 @@ for (i in i.index) {
   # Update the V vector of option values at each time step and node
   V = pmax(0, discount * diag(E_V%*%prob))
   
-  if (i==0){Forw = Forw[1]; V= V[1]}
+  if (i==0){
+    Forw = Forw[1]
+    V= V[1]
+    result1 = as.data.frame(V); rownames(result1) = "Result 1:"; colnames(result1) = "Option Value"
+  }
   # The following print out is useful for a trinomial function
   # It will print out the results as the function is running to keep track
   cat("Time step: ", i, "\n", sep="")
@@ -63,7 +67,7 @@ for (i in i.index) {
 }
 
 
-##### Value AMERICAN CALL Option ####
+##### Problem 2: Value AMERICAN CALL Option ####
 mult = 1 #call, not put. Put is valued at K-S.
 # i) Fitted forward prices, spot price data fitted correctly
 Forw = exp(as.numeric(a_is[length(j.index)]) + level_x)
@@ -96,7 +100,11 @@ for (i in i.index) {
   # Now value is either exercising option or EV of not exercising
   V = pmax(mult*(Forw-tree_Params$K), discount * diag(E_V%*%prob))
   
-  if (i==0){Forw = Forw[1]; V= V[1]}
+  if (i==0){
+    Forw = Forw[1]
+    V= V[1]
+    result2 = as.data.frame(V); rownames(result2) = "Result 2:"; colnames(result2) = "Option Value"
+  }
   # The following print out is useful for a trinomial function
   # It will print out the results as the function is running to keep track
   cat("Time step: ", i, "\n", sep="")
@@ -109,7 +117,7 @@ for (i in i.index) {
 }
 
 
-##### Value AMERICAN PUT Double Barrier Knock-out Option ####
+##### Problem 3: Value AMERICAN PUT Double Barrier Knock-out Option ####
 mult = -1 #call, not put. Put is valued at K-S.
 barrier = c(16, 30)
 # i) Fitted forward prices, spot price data fitted correctly
@@ -147,7 +155,11 @@ for (i in i.index) {
   V = pmax(mult*(Forw-tree_Params$K), discount * diag(E_V%*%prob))
   V[KOed] <- 0
   
-  if (i==0){Forw = Forw[1]; V= V[1]}
+  if (i==0){
+    Forw = Forw[1]
+    V= V[1]
+    result3 = as.data.frame(V); rownames(result3) = "Result 3:"; colnames(result3) = "Option Value"
+  }
   # Print out the results as the function is running to keep track
   cat("Time step: ", i, "\n", sep="")
   cat("Prices:\n")
@@ -157,6 +169,54 @@ for (i in i.index) {
   
   offset = offset +1 
 }
+
+
+##### Problem 4: 3-level (indexing at 0) trinomial tree solution #####
+#What do we need to fill out the 3rd level of the tree?
+# simplified tree price levels
+# Option values (i.e. discounted expected value of option values at theoretical j = 4 level)
+# transition probabilities to go from level 3 to theoretical j = 4 level
+# State prices at level 3.
+
+params = S4_Params[,c(1:4)]
+alpha = 0.338; sigma = 0.305
+dt = 0.083; dx = 0.153; r = params["R_ts",1]
+#simplified prices at level 3:
+level_x3 = dx*seq(from = -3, to = 3, by = 1)
+nbNodes = 7 #number of nodes at 3rd level
+j = 3
+
+#transition probabilities: From Thomas Fillebeen
+prob <- function(x, dt, alpha, dx, vol, K){ 
+  pu =  1/2*((vol^2*dt + alpha^2*(x^2)*dt^2)/dx^2) -
+          ((alpha*x*dt)/dx)
+  pd = 1/2*((vol^2*dt + alpha^2*(x^2)*dt^2)/dx^2) +
+          ((alpha*x*dt)/dx)
+  pm = 1 - pu - pd
+  prob = list(pu = pu, pm = pm, pd = pd)
+  return(prob)
+}
+
+# transition probs
+level_x2 = dx*seq(from = -2, to = 2, by = 1)
+
+# Calculate a_3 so we can get option values at j = 3
+df = exp(-S4_Params["R_ts",1]*dt)
+Q_2 = c(0.0252, 0.2199, 0.4998, 0.2199, 0.0252)
+#Want Q_level3: will have 7 nodes.
+Q_3 = numeric(length = nbNodes)
+#according to "State prices continued" slide
+P_2 = c(.1401, .6635, .1964, .1530, .6659, .1811,
+        .1667, .6667, .1667, .1811, .6659, .1530,
+        .1964, .1530, .6635, .1401)
+
+Q_3[1] = Q_2[1]*P_2[1]*df
+Q_3[nbNodes] = Q_3[1]
+Q_3[2] = Q_2[1]*P_2[2]*df + Q_2[2]*P_2[4]*df
+Q_3[nbNodes-1] = Q_3[2]
+Q_3[3] = Q_2[1]*P_2[3]*df + Q_2[2]*P_2[5]*df + Q_2[3]*P_2[7]*df
+Q_3[nbNodes-2] = Q_3[3]
+Q_3[4] = Q_2[2]*P_2[6]*df + Q_2[3]*P_2[8]*df + Q_2[4]*P_2[10]*df
 
 
 
